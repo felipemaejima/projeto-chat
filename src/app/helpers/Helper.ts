c085e1a32aa3bc0914954ef0602d4507b418daf4
felipemaejima <felipemaejima@gmail.com>
2024-06-02 16:52:48 -0300

    função helper checkToken

--- src/app/helpers/Helper.ts
+++ src/app/helpers/Helper.ts
@@ -1,4 +1,16 @@
+import Security from "../../infrastructure/security/Security";
+
 export function isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+}
+export async function checkToken(token: string | null): Promise<boolean> {
+	if (!token) return false;
+
+	const sessionToken = token?.split(" ")[1];
+	const isValidToken = await Security.verifyTokenValidity(sessionToken);
+
+	if (!isValidToken) return false;
+
+	return true;
 }
