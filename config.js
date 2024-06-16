03e74981eb02d23fc91b1d22d10b9df1b7eaf3fb
felipemaejima <felipemaejima@gmail.com>
2024-06-16 14:17:51 -0300

    ajuste config.js e package.json

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
     dbHost: process.env.DB_HOST || "localhost",
     dbUser: process.env.DB_USER || "root",
     dbPass: process.env.DB_PASS || "",
-    dbName: process.env.DB_NAME || "api_db",
+    dbName: process.env.DB_NAME || "chat",
     JWTSecret: process.env.JWT_SECRET || "secret",
     sessionTime: process.env.SESSION_TIME || 30,
     passMinLength: process.env.PASSWORD_MIN_LENGTH || 3, 
